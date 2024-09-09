88d390c02bbe9ef799864e73d53d7d637bc99eb9
SAGRSURI <sagarsuriofficial@gmail.com>
2024-09-09 16:04:33 +0530

    V.1.0.0

--- src/Components/Blog/BlogDetail.jsx
+++ src/Components/Blog/BlogDetail.jsx
@@ -4,7 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import blogData from './blogData';
 import {GistViewer} from 'vite-gist';
 import ToggleO from '../../OwnedComponents/ToggleO';
-
+import Back from '../Loaders/Rippling/RipplingBack'
 
 
 function BlogDetail() {
@@ -16,51 +16,52 @@ function BlogDetail() {
   }
 
   return (
-    <div className="min-h-screen h-full">
-      <div className='flex justify-center gap-1 px-6 py-4'>
+    <div className="min-h-screen w-full">
+      <div className='flex justify-center gap-1 py-4'>
         <button>
           <Link to={'/blog'} className='fill-left-to-right px-6 py-1 rounded-s-full text-xl flex justify-center border-[1px] border-black dark:border-white bg-black text-white hover:text-black dark:bg-white dark:text-black dark:hover:text-white'>Back</Link>
         </button>
-        <ToggleO/>
+        <ToggleO />
       </div>
 
-      
-      <div className="container mx-auto px-4 py-4">
-        <h1 className="text-4xl font-bold mb-4">{blog.title}</h1>
+
+      <div className="container mx-auto px-1 py-4">
+        <h1 className="text-4xl text-center uppercase font-bold mb-6">{blog.title}</h1>
         <img src={blog.image} alt={blog.title} className="w-full h-64 object-cover mb-4" />
-       <Link to={blog.projectLink}><h1 className='text-center bg-green-400 inline my-1 mx-auto w-full font-bold p-1 rounded-md'>VIEW LIVE</h1></Link>
-        <p className="text-gray-600 dark:text-gray-400 mb-4">{blog.content}</p>
+        <Link to={blog.projectLink}><h1 className='text-center flex justify-center bg-green-400 w-1/5 my-1 mx-auto font-bold p-1 rounded-md'>VIEW LIVE</h1></Link>
+        <p className="capitalize my-6">{blog.content}</p>
 
         <div className='w-full'>
-        <GistViewer
-        gistId={blog.gistId}
-        containerClassName="bg-white dark:bg-black p-1"
-        buttonClassName=" text-black dark:text-white dark:bg-black mx-2 p-1 rounded-md"
-        fontSizeButtonContainerClassName="flex justify-between px-8"
-        toggleButtonClassName="text-xl dark:text-white dark:bg-black p-1 rounded-md"
-        loaderClassName="text-center p-3 m-2 font-bold text-xl text-gray-600 dark:text-white"
-        codeContainerClassName="p-4 rounded-md"
-        themeStyle={null}
-        showLineNumbers={true}
-        fontSize="1rem"
-        codeBackgroundColor="#f5f5f5"
-      />
-        </div>
-
-        {/* Embed the YouTube video if present */}
+          <GistViewer
+            gistId={blog.gistId}
+            containerClassName="bg-white dark:bg-black p-1"
+            buttonClassName=" text-black dark:text-white dark:bg-black mx-2 p-1 rounded-md"
+            fontSizeButtonContainerClassName="flex justify-between px-8"
+            toggleButtonClassName="text-xl dark:text-white dark:bg-black p-1 rounded-md"
+            loaderClassName="text-center p-3 m-2 font-bold text-xl text-gray-600 dark:text-white"
+            codeContainerClassName="p-4 rounded-md"
+            themeStyle={null}
+            showLineNumbers={true}
+            fontSize="1rem"
+            codeBackgroundColor="#f5f5f5"
+          />
+          {/* Embed the YouTube video if present */}
         {blog.youtubeLink && (
           <div className="relative mb-4" style={{ paddingTop: '56.25%' }}>
-            <iframe 
+            <iframe
               className="absolute top-0 left-0 w-full h-full"
-              src={blog.youtubeLink} 
-              title="YouTube Video" 
-              frameBorder="0" 
+              src={blog.youtubeLink}
+              title="YouTube Video"
+              frameBorder="0"
               allowFullScreen
               style={{ border: 'none' }}
             ></iframe>
           </div>
         )}
-        <Link to="/blog" className="text-blue-500 hover:text-blue-700 dark:hover:text-blue-400">Back to Blog List</Link>
+        </div>
+      </div>
+      <div className='flex justify-center w-full'>
+      <Back/>
       </div>
     </div>
   );
--- src/Components/Blog/blogData.js
+++ src/Components/Blog/blogData.js
@@ -6,8 +6,8 @@ const blogData = [
     id: '1',
     title: 'CREATED NPM PACKAGE FOR GITHUB GIST VIWE',
     image: gistImage,
-    excerpt: 'This is a brief overview of the first blog post. Click to read more.',
-    content: 'The Below code is a vite@latest project component , you can use this with create Toggle.jsx and import that code there , then you don"t need to apply dark mode anyware he can apply you need to just install tailwind css in our project ',
+    excerpt: "Below the YouTube video. I've pro...",
+    content: "Below the YouTube video, I've provided a detailed explanation on how to use the GitHub GistViewer npm package. Additionally, a comprehensive README file is included to guide you through the process. I've developed the vite-gist package, a powerful and flexible Gist viewer component for React applications. Simply install the package, import the vite-gist component, and follow the instructions provided here or in the npm package documentation to integrate it into your project. This package stands out due to its advanced features. I couldn't find any existing solution that met my needs, so I built this from scratch. With vite-gist, you're free to fully customize the appearance of the Gist viewer, including support for both light and dark modes. It also offers a font resizing option for an enhanced user experience. Whether you're looking for flexibility or ease of use, this package provides a comprehensive solution for embedding and customizing GitHub Gists in your React apps.",
     gistId: '00120b53c86355f0b9b42e422a2d106d', // Use Gist ID here
     gistLink: 'https://gist.github.com/SagrSuri/0606d142d5573cf79f91126bc4ebabc0',
     youtubeLink: 'https://www.youtube.com/embed/qUHokTXF1M0?si=bKt4BEQDQCE9rnfS',
@@ -15,11 +15,11 @@ const blogData = [
   },
   {
     id: '2',
-    title: 'Toggle.jsx component for DarkMode',
+    title: 'dark Mode toggle component for React Projects',
     image: toggleImage,
     excerpt: 'This is a brief overview of the first blog post. Click to read more.',
-    content: 'The Below code is a vite@latest project component , you can use this with create Toggle.jsx and import that code there , then you don"t need to apply dark mode anyware he can apply you need to just install tailwind css in our project ',
-    gistId: '0606d142d5573cf79f91126bc4ebabc0', // Use Gist ID here
+    content: "I've developed an npm package for a Dark Mode toggle button, which you can easily integrate into your React projects. Below the YouTube video, you'll find a detailed walkthrough on how to use the package. Additionally, a complete README file is included with clear instructions for installation and usage. To get started, simply install the package, then refer to the README for step-by-step guidance. The package is also documented on the npm website, where you'll find all the essential details. A live demo is available—just click the link and experience it in action This package is designed for simplicity and flexibility, allowing seamless integration of Dark Mode functionality into your projects.",
+    gistId: '78996f71db49dab4093e982b554551e7', // Use Gist ID here
     gistLink: 'https://gist.github.com/SagrSuri/0606d142d5573cf79f91126bc4ebabc0',
     youtubeLink: 'https://www.youtube.com/embed/qUHokTXF1M0?si=bKt4BEQDQCE9rnfS',
     url: '/blog/my-first-blog-post',
