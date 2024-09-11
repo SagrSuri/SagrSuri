87f17c3bdce7c0ca4e611826fbb1cd505588ab88
SAGRSURI <sagarsuriofficial@gmail.com>
2024-09-11 19:49:18 +0530

    WelcomeImageChange.

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -11,7 +11,7 @@ function Welcome() {
   const externalImageUrlComputerDark = 'https://img.freepik.com/premium-photo/word-welcome-isolated-white-background-made-uncial-calligraphy-style_107173-64339.jpg?w=826';
   const externalImageUrlComputerLight = 'https://img.freepik.com/premium-vector/welcome-ready-inscription-template-neon-signboard_110464-103.jpg?w=740';
 
-  const externalImageUrlMobileTabletDark = 'https://img.freepik.com/premium-photo/colorful-welcome-sign-that-says-welcome_1282156-6862.jpg?w=740';
+  const externalImageUrlMobileTabletDark = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNTZqO1pgf4LOExXm_FvzlAHopOekof5JppQ&s';
   const externalImageUrlMobileTabletLight = 'https://img.freepik.com/free-vector/welcome-neon-sign-vector_53876-76088.jpg?t=st=1725373247~exp=1725376847~hmac=ebb91d36ee8b7aa3545eb2a5bb87026cc8fef9d6db93614a8b64b4ed2635788a&w=360';
 
   useEffect(() => {
